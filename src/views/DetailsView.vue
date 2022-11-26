6029c4d0940176fa37e4ba6fa999480a2b340454
dimitri-donatien <donatien.dim@gmail.com>
2022-11-26 01:15:35 +0100

    update component

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -1,30 +1,60 @@
 <template>
-    <div class="details">
-     <side-comp></side-comp>
-     <main class="details__container">
-      <banner-comp></banner-comp>
-        <section class="contact__content">
-        
-        </section>
+  <div class="details">
+    <side-comp></side-comp>
+    <main class="details__container">
+      <banner-comp :title="film.title"></banner-comp>
+      <section class="details__content">
+        <template v-if="isErrored">
+          <p>
+            We're sorry, we are unable to retrieve this information at this
+            time. Please retry later.
+          </p>
+        </template>
+
+        <template v-else>
+          <div v-if="isLoaded" class="loading">Loading...</div>
+
+          <article v-else>
+            <h1>work in progress</h1>
+          </article>
+        </template>
+      </section>
       <footer-comp></footer-comp>
-     </main>
-   </div>
- </template>
- 
- <script setup>
- import SideComp from '@/components/layout/SideComp'
- import BannerComp from '@/components/layout/BannerComp'
- import FooterComp from '@/components/layout/FooterComp'
- 
- </script> 
- 
- <style lang="scss" scoped>
- .details {
+    </main>
+  </div>
+</template>
+
+<script setup>
+import SideComp from "@/components/layout/SideComp";
+import BannerComp from "@/components/layout/BannerComp";
+import FooterComp from "@/components/layout/FooterComp";
+
+import { onMounted, onUnmounted, computed } from "vue";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
+
+const store = useStore();
+const route = useRoute();
+
+const film = computed(() => store.state.ghibli.film);
+const isLoaded = computed(() => store.state.ghibli.loading);
+const isErrored = computed(() => store.state.ghibli.error);
+
+onMounted(() => {
+  store.dispatch("ghibli/getFilm", route.params.id);
+});
+onUnmounted(() => {
+  store.commit("ghibli/resetFilm");
+});
+</script>
+
+<style lang="scss" scoped>
+.details {
   display: flex;
-  background-color: var(--background-color-primary);
-  color: var(--text-primary-color);
-  max-height: 100vh;
+  background-color: var(--background-color-secondary);
   width: 100%;
+  height: 100vh;
+  max-height: 100vh;
   overflow: hidden;
 }
 .details__container {
@@ -34,4 +64,11 @@
   padding: 1.5rem;
   overflow-y: auto;
 }
- </style>
+.details__content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem 0;
+}
+</style>
